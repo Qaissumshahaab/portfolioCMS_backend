34fce10592faf1b3f4d57528c396b5b4b7aac474
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-08-01 17:55:11 +0500

    review and fix verifyUseraccesstoken.js in middleware

--- middleware/verifyUseraccesstoken.js
+++ middleware/verifyUseraccesstoken.js
@@ -9,7 +9,7 @@ const verifyUseraccesstoken = (req, res, next) => {
   } catch (error) {
     return res.status(401).json({ success: false });
     //401 go to frontend and take by axios intercepetor and get request to /refersh-token
-    // also solve refresh race edge case as 5 simultaneos request make 1st refersh token acccessable and second mismatch
+    // i strugle here so writing this coment so i can refrence for future and come and see here what i do to tackle it :also solve refresh race  as 5 simultaneos request make 1st refersh token acccessable and second mismatch
   }
 };
 

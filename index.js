e4a9727dd56b5d323be22fb8347b85f124680ddc
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-08-01 20:19:42 +0500

    added dotenv import and config dotenv in index.js at top of the file to ensure that environment variables are loaded before the app starts.
    this was a real painful bug to find, because the app was loading first and then dotenv was loading, so the environment variables were not available when the app loaded, which broke the functionality.

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
+import "dotenv/config"; // must be the first import, otherwise app load first and then dotenv will load and the env variables will not be available when app loaded so functionailty broken.
 import app from "./App.js";
-import dotenv from "dotenv";
 import connectDB from "./config/mongoconfig.js";
-
-dotenv.config({ quiet: true });
 
 const port = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
-    app.listen(port, () => {
-      console.log("App is listening on port :" + port);
-    });
+    app.listen(port, () => console.log("App is listening on port :" + port));
   })
-
   .catch((error) => {
     console.error("Error while connecting to port" + error.message);
   });

239008a5a360aab10b479d9bce6d6e7387ceea18
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-08-08 19:58:33 +0500

    fix bug in config/mongoconfig.js that was after connection drop there was no way to check is it droped as varibale isconnected was always set to true after 1st connection established

--- config/mongoconfig.js
+++ config/mongoconfig.js
@@ -1,17 +1,12 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 const connectDB = async () => {
-  if (isConnected) {
+  if (mongoose.connection.readyState === 1) {
     return;
   }
 
   try {
     const connection = await mongoose.connect(process.env.MONGODB_URL);
-
-    isConnected = connection.connections[0].readyState === 1;
-
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
